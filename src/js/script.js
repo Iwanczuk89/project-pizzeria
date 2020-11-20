724c905bca2276470d29190c55bb084931ff5813
Andrzej Iwańczuk <Iwanczuk89@gmail.com>
2020-11-20 22:45:13 +0100

    add const images to processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -92,6 +92,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -202,7 +203,21 @@
       
             /* END ELSE IF: if option is not selected and option is default */
           }
-    
+          
+          const images = thisProduct.imageWrapper.querySelectorAll('.' + paramId + '-' + optionId);
+          console.log(images);
+
+          if(optionSelected) {
+            for(let image of images) {
+              image.classList.add(classNames.menuProduct.imageVisible);
+            }
+          }
+          else{
+            for(let image of images) {
+              image.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
+          
           /* END LOOP: for each optionId in param.options */
         }
   
